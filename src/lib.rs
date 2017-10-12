099f968c0bb6a295e263004ae5c68ccad10c62c6
Dimeji Ogunyoye <dimejiogunyoye@googlemail.com>
2017-10-12 09:45:02 +0100

    Adding watch_with_closure implementation

--- src/lib.rs
+++ src/lib.rs
@@ -67,6 +67,15 @@ pub fn watch_with_callback(callback: &ProcessWatcherCallback) -> () {
     }
 }
 
-pub fn watch_with_closure(on_open: &Fn(i32) -> (), on_close: &Fn(i32) -> ()) {
-    //Implement me
+pub fn watch_with_closure(on_open: &Fn(u32) -> (), on_close: &Fn(u32) -> ()) {
+    let mut sets = ProcessSets { prev_set: HashSet::new(), curr_set: HashSet::new() };
+    let changed_sets = get_processes(&mut sets);
+
+    for open in changed_sets.0.iter() {
+        on_open(open.clone());
+    }
+
+    for close in changed_sets.1.iter() {
+        on_close(close.clone());
+    }
 }
